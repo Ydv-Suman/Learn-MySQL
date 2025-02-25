a56817150502005ae0090d1caa90c3534b9ded27
Ydv-Suman <sumanydv615@gmail.com>
2025-02-25 13:09:28 -0600

    table_DateTime

---
+++ myDB/table_datetime/creatingdatetime_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE date_time(
+	my_date DATE,
+    my_time TIME,
+    my_dateTime DATETIME
+);
---
+++ myDB/table_datetime/inserting_value.sql
@@ -0,0 +1,8 @@
+INSERT INTO date_time
+VALUES(
+	CURRENT_DATE(),
+    CURRENT_TIME(),
+    NOW()
+);
+
+SELECT * FROM date_time
